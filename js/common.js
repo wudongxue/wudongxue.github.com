4496c7b15a7ba8e1d5b73ea4f62b80a63f3808f7
Shi Kailun <shikailun@gmail.com>
2013-12-24 01:26:57 +0800

    弹出层

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 $(function() {
-
+	$('.btn_download').click(function(){
+		$.ajax({
+            url: "./data/dx.txt",
+            success: function(data){
+                $.facybox(data);
+            }
+        });
+		return false;
+	});
 });
