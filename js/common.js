a2105f520f3e0e20dfe5eaa3ab2089e071b65bf2
Shi Kailun <shikailun@gmail.com>
2013-12-24 18:24:20 +0800

    基础版

--- js/common.js
+++ js/common.js
@@ -1,11 +1,27 @@
 $(function() {
-	$('.btn_download').click(function(){
+	$('.btn_download').click(function() {
 		$.ajax({
-            url: "./data/dx.txt",
-            success: function(data){
-                $.facybox(data);
-            }
-        });
+			url: "./data/dx.html",
+			success: function(data) {
+				$.facybox(data);
+			}
+		});
 		return false;
 	});
+	$('img').attr('unselectable', 'on')
+		.css({
+			'-moz-user-select': '-moz-none',
+			'-moz-user-select': 'none',
+			'-o-user-select': 'none',
+			'-khtml-user-select': 'none',
+			'-webkit-user-select': 'none',
+			'-ms-user-select': 'none',
+			'user-select': 'none'
+		}).bind('selectstart', function() {
+			return false;
+		}).bind('mousedown',function(){
+			return false;
+		}).bind('mousemove',function(){
+			return false;
+		});
 });
